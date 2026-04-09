46790fc7e61ddfd36f1617bb51e0c0bf2f1cce62
shairacastro <shairacastro08@gmail.com>
2026-04-09 06:58:12 -0600

    Update IntroCard.js

--- src/components/IntroCard.js
+++ src/components/IntroCard.js
@@ -11,7 +11,7 @@ export default function IntroCard({ intro }) {
         name,
         summary,
         socialButtons = [],
-        supportLink,
+        supportLink = {},
         image,
     } = intro;
 
